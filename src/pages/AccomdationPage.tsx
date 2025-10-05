a27e027ee9c848b8bc78f8ba12b4cc97971a2304
Dhanushka Mapagedara <mapagedaradlb.20@uom.lk>
2025-10-05 12:48:08 +0530

    devolopeaccomadaion filter  page

---
+++ src/pages/AccomdationPage.tsx
@@ -0,0 +1,15 @@
+
+
+const AccomdationPage = () => {
+  return (
+    <div className="p-5 bg-pink-500 min-h-screen">
+      <div className="bg-blue-950 p-5 text-white align-items-center justify-content-center"></div>
+  <h1>Find your next stay</h1>
+  <p>Search low prices on hotels, homes and much more...</p>
+    </div>
+
+    
+  )
+}
+
+export default AccomdationPage
--- src/pages/TourPage.tsx
+++ src/pages/TourPage.tsx
@@ -11,11 +11,27 @@ const TourPage = () => {
         {
             tour?(
                
-                <div>
-                    <img src={tour.image} alt={tour.name} className='w-full h-96 object-cover mt-3'/>
-                      <h1 className='text-3xl font-bold mt-3'>{tour.name}</h1>
-                        <p className='text-gray-600 mt-2'>{tour.overview}</p>
-                        <h2 className='text-2xl font-bold mt-5'>Places to Visit</h2>
+                <div className=''>
+  
+<img src={tour.image} alt={tour.name} className='w-full h-96 object-contain mt-3'/>
+
+                    <div className='flex align-items-center justify-content-between mt-1 gap-5'>
+                      <div>
+  <h1 className='text-3xl font-bold mt-3'>{tour.name}</h1>
+   <p className='text-gray-600 mt-2'>{tour.overview}</p>
+</div>
+          <div className='mt-3 p-5 border-2 border-purple-500 rounded-lg h-1/2 bg-purple-600'>
+                      <h3>{tour.days} Days</h3>
+                    
+                    </div>
+
+
+
+                      </div>  
+
+<div className='flex align-items-center justify-content-between gap-8 mt-5'>
+  <div>
+<h2 className='text-2xl font-bold mt-5'>Places to Visit</h2>
                         <ul className='list-disc list-inside mt-2 text-gray-600'>
                             {tour.places.map((place, index) => (
                                 <li key={index}>{place}</li>
@@ -30,7 +46,18 @@ const TourPage = () => {
                                 </li>
                             ))}
                         </ul>
-                </div>
+  </div>
+  <div>
+    <button className="p-2 bg-purple-600 text-white rounded-lg position-absolute mt-15 w-1/2 cursor-pointer ">Get your Vehicle & Accommodation Reservation</button>
+  </div>
+ <div>
+  
+ </div>
+</div>
+                       
+                  </div>
+                    
+                
               
             ):
         (<div className='text-red-500 text-2xl'>Tour Not Found</div>)
